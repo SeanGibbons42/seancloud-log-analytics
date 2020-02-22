0712b797ebd7a909ad5fbcb376b6b607a6481751
Sean Gibbons <seangibbon42@gmail.com>
2020-02-21 22:07:13 -0500

    Frontend Toolchain

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/frontend/index.js',
+
+  output: {
+    path: path.join(__dirname, 'dist', 'frontend'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      //tell process all javascript and jsx files with babel
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+    ]
+  },
+
+  plugins: [
+    new htmlWebpackPlugin({
+      title: "Webpack Output",
+      template: "dist/frontend/index.html",
+      inject: false
+    }),
+  ],
+
+  // setup for the auto-refresh server
+  devServer: {
+    contentBase: './dist/frontend',
+    open: true,
+  }
+
+}
